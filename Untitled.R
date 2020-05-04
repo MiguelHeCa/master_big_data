61934100bcd10dc32f3c49d56e67847585608495
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2020-05-04 16:04:40 +0200

    last exam

---
+++ Untitled.R
@@ -0,0 +1,44 @@
+---
+title: "Bibliografía STATIS y TUCKER"
+author: 
+  - José Miguel Hernández Cabrera
+  - Laura Gil García
+output: html_document
+lang: es
+cls: https://raw.githubusercontent.com/citation-style-language/styles/master/apa.csl
+bibliography: tucker.bib
+---
+
+# Métodos
+
+The most common methods are the Triadic Analysis [39], from the STATIS family methods [23,31],
+
+J. Thioulouse and D. Chessel, Les analyses multitableaux en écologie factorielle. I. De la typolo- gie d’état à la typologie de fonctionnement par l’analyse triadique, Acta Oecologica, Oecologia Generalis 8 (1987), pp. 463–480.
+
+
+the PARAFAC model [15] 
+
+
+and Tucker models [43]. 
+
+
+The Triadic Anal- ysis seeks a consensus configuration over time or space and therefore aims to evaluate the stable part in the dynamics of relationships, 
+
+
+while the PARAFC model is the basis of the three-way factor analysis. 
+
+
+On the other hand, in the Tucker methods, models are adjusted to achieve the goal of explaining the interaction. 
+
+
+STATICO is much valued by researchers, given the poten- tial associated with it (since it provides a complete and consistent analysis framework, with a stronger mathematical background [41] Nevertheless, this method ‘only’ displays the stable component of data relationship variations, without revealing what is different or interacts (such as Tucker models, which puts in evidence by the core matrix and the related joint biplots)
+
+On the other hand, three-mode models, as the Tucker3 model, has for objec- tive to represent measured data as a linear combination of a small number of optimal, orthogonal components following an appropriate generalization of PCA applied to multi- way data arrays. Thus, the Tucker3 model can lead to an easier and more straightforward interpretation of the relevant information contained in a three-way data set. Actually, one of the major advantages of modeling three-way data sets with three-mode models is the gain in interpretability.
+
+CO-TUCKER, that combines Co-Inertia Analysis and Tucker3 to analyze two paired k-tables. It is compared to STATICO
+
+TUCKER3 TUCKER3 model is achieved via an alternating least squares algorithm which minimizes a loss function.
+
+# Bibliografía
+
+
